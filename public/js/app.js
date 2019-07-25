b2c3ea757e25308c3e340273766d1eefd1cbe0a4
Joe Karanka <joekaranka@Joes-iMac.local>
2019-07-26 00:02:21 +0300

    editted ports and run commands

--- public/js/app.js
+++ public/js/app.js
@@ -24,7 +24,7 @@ weatherForm.addEventListener('submit', (e) => {
    console.log(search.value)
    const location = search.value;
 
-   fetch('http://localhost:3000/weather?address=' + location )
+   fetch('/weather?address=' + location )
       .then((response) => {
          response.json().then((data) => {
             if (data.error) {
